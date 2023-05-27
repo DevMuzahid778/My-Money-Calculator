131c8c614671c078a9b42f4350b4c17ecc39c449
Muzahid <mdmuzahid.gd@gmail.com>
2023-05-27 17:52:35 +0600

    Total expense features added

---
+++ script.js
@@ -0,0 +1,18 @@
+document.getElementById("Calculate").addEventListener("click", function(){
+    const Income = document.getElementById("Income");
+    const IncomeAmount = parseFloat(Income.value);
+
+    const Food = document.getElementById("Food");
+    const FoodCost = parseFloat(Food.value);
+
+    const Rent = document.getElementById("Rent");
+    const RentCost = parseFloat(Rent.value);
+
+    const Clothes = document.getElementById("Clothes");
+    const ClothesCost = parseFloat(Clothes.value);
+
+    const TotalExpense = FoodCost + RentCost + ClothesCost;
+    const TotalCost = document.getElementById("TotalCost")
+    TotalCost.innerText = TotalExpense;
+    
+})
