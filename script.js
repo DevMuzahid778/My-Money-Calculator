8fd5910dccc34e9620259a3a09a2dabc01e13caf
Muzahid <mdmuzahid.gd@gmail.com>
2023-05-27 18:40:45 +0600

    Saving Balance, Remaining Balance added

--- script.js
+++ script.js
@@ -15,9 +15,32 @@ document.getElementById("Calculate").addEventListener("click", function(){
     const TotalCost = document.getElementById("TotalCost")
     TotalCost.innerText = TotalExpense;
 
-
     const NewBalance = IncomeAmount - TotalExpense;
     const Balance = document.getElementById("Balance");
     Balance.innerText = NewBalance;
+
+    Food.value = "";
+    Rent.value = "";
+    Clothes.value = "";
     
+})
+// Saving Balance & Remaining Balance functionality added
+document.getElementById("SaveMoney").addEventListener("click", function(){
+    const Balance = document.getElementById("Balance");
+    const TotalBalance = parseFloat(Balance.innerText);
+
+    const Saving = document.getElementById("Saving");
+    const SavingAmount = parseFloat(Saving.value);
+    const savingparcent = SavingAmount/100;
+    const SavingBalance = TotalBalance*savingparcent;
+
+    const savefiled = document.getElementById("SavingAmount");
+    savefiled.innerText = SavingBalance;
+
+    const RemainBalance = document.getElementById("RemainBalance");
+    const RemainTotalBalance = TotalBalance - SavingBalance;
+    RemainBalance.innerText = RemainTotalBalance;
+
+    Saving.value ="";
+    document.getElementById("Income").value = "";
 })
